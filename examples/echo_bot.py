27bb9b47dcb48ae3c92afd569468ef2321eb0de2
Ahmet Burhan Kayalı <82338831+SoAp9035@users.noreply.github.com>
2024-08-16 20:40:29 +0300

    Add files via upload

---
+++ examples/echo_bot.py
@@ -0,0 +1,26 @@
+from simple_telegram_api import TelegramBot
+
+BOT_TOKEN = "BOT_TOKEN"
+
+bot = TelegramBot(BOT_TOKEN)
+
+# Skip old messages before bot is running.
+bot.reset_updates()
+
+print("Bot is running.")
+while True:
+    updates = bot.get_updates()
+
+    # Check if it's empty.
+    if updates:
+        print(updates)
+        bot.reset_updates(updates=updates)
+
+        # For multiple coming up messages.
+        for update in updates["result"]:
+            chat_id = update["message"]["chat"]["id"]
+            message_id = update["message"]["message_id"]
+            user_text = update["message"]["text"]
+            
+            bot_message = bot.send_message(chat_id=chat_id, text=user_text)
+
